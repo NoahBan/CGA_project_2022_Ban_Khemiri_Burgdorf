b25088f77e6194e316127c9b295db500a4688232
NoahBan <noah_kyeong-seo.ban@smail.th-koeln.de>
2022-06-12 19:57:22 +0200

    Add the other cringe shit

---
+++ project/src/main/kotlin/cga/exercise/components/geometry/Material.kt
@@ -0,0 +1,16 @@
+package cga.exercise.components.geometry
+
+import cga.exercise.components.shader.ShaderProgram
+import cga.exercise.components.texture.Texture2D
+import org.joml.Vector2f
+
+class Material(var diff: Texture2D,
+               var emit: Texture2D,
+               var specular: Texture2D,
+               var shininess: Float = 50.0f,
+               var tcMultiplier : Vector2f = Vector2f(1.0f)){
+
+    fun bind(shaderProgram: ShaderProgram) {
+        // todo 3.2
+    }
+}
